fb58336f2845f35f4c925bad287e5ae04bc56209
Ankur Seth <iankurseth@gmail.com>
2023-12-06 15:53:01 +0530

    Added a 60 dash line with Dash component

--- demo/src/App/Main/AnalogClock/AnalogClock.tsx
+++ demo/src/App/Main/AnalogClock/AnalogClock.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ClockHand from "./ClockHand";
 import Dot from "./Dot";
+import Dash from "./Dash";
 
 
 
@@ -43,13 +44,14 @@ export default function AnalogClock ({time}: AnalogClockProps) {
 		<section className="bg-white relative p-4">
 			<section style={style} className="bg-zinc-100 w-full h-full rounded-full">
 				<section className="absolute top-1/2 left-1/2 w-0 h-0">
-					<ClockHand deg={h*30} color="#222" className="rounded-t-full" width={20} length={150} />
-					<ClockHand deg={m*6} color="blue" className="rounded-t-full" width={15} length={220} />
-					<ClockHand deg={s*6} color="red" className="rounded-t-full" width={10} length={270} />
+					<ClockHand deg={h*30} color="#222" className="rounded-t-full" width={20} length={radius * 0.45} />
+					<ClockHand deg={m*6} color="blue" className="rounded-t-full" width={15} length={radius * 0.70} />
+					<ClockHand deg={s*6} color="red" className="rounded-t-full" width={10} length={radius * 0.85} />
 
 					<div className="w-10 h-10 bg-zinc-900 rounded-full shadow -translate-x-1/2 -translate-y-1/2"></div>
 
 					{sixty.map(idx => <Dot key={idx} deg={idx * 6} radius={radius} size={(idx % 5 === 0) ? 16 : 8} />)}
+					{sixty.map(idx => <Dash key={idx} deg={idx * 6} radius={radius * 0.45} />)}
 				</section>
 			</section>
 		</section>
---
+++ demo/src/App/Main/AnalogClock/Dash.tsx
@@ -0,0 +1,35 @@
+
+
+
+interface DashProps {
+	deg: number,
+	radius: number,
+	height?: number,
+	width?: number,
+	className?: string,
+	color?: string
+}
+
+export default function Dash ({
+	deg, radius,
+	height=16,
+	width=4,
+	className="",
+	color="#111"
+}: DashProps) {
+	const radians = deg * Math.PI / 180;
+	const hyp = radius;
+
+	const style = {
+		backgroundColor: color,
+		height: `${height}px`,
+		width: `${width}px`,
+		top: `${Math.cos(radians) * hyp}px`,
+		left: `${Math.sin(radians) * hyp}px`,
+		transform: `translateX(-50%) translateY(-50%) rotateZ(-${deg}deg)`
+	};
+
+	return (
+		<div style={style} className={`absolute rounded-full ${className}`}></div>
+	);
+}
