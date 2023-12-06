a4096f3e43863fb83dc45e0ef2fd7e1ea348d843
Ankur Seth <iankurseth@gmail.com>
2023-12-06 16:00:04 +0530

    Now hour and minute can be non-integers

--- demo/src/App/Main/AnalogClock/AnalogClock.tsx
+++ demo/src/App/Main/AnalogClock/AnalogClock.tsx
@@ -14,12 +14,9 @@ export default function AnalogClock ({time}: AnalogClockProps) {
 	const radius = Math.floor(size / 2);
 
 	const seconds = Math.floor(time / 1000);
-	const minutes = Math.floor(seconds / 60);
-	const hours = Math.floor(minutes / 60);
-
 	const s = seconds % 60;
-	const m = minutes % 60;
-	const h = hours % 12;
+	const m = seconds / 60;
+	const h = seconds / 3600;
 
 	const updateSize = () => {
 		const wh = window.innerHeight;
@@ -39,19 +36,22 @@ export default function AnalogClock ({time}: AnalogClockProps) {
 	};
 
 	const sixty = [...Array(60).keys()];
+	const hoursHandLength = radius * 0.45;
+	const minutesHandLength = radius * 0.70;
+	const secondsHandLength = radius * 0.85;
 
 	return (
-		<section className="bg-white relative p-4">
-			<section style={style} className="bg-zinc-100 w-full h-full rounded-full">
+		<section className="bg-zinc-100 relative p-4">
+			<section style={style} className="bg-white w-full h-full rounded-full shadow">
 				<section className="absolute top-1/2 left-1/2 w-0 h-0">
-					<ClockHand deg={h*30} color="#222" className="rounded-t-full" width={20} length={radius * 0.45} />
-					<ClockHand deg={m*6} color="blue" className="rounded-t-full" width={15} length={radius * 0.70} />
-					<ClockHand deg={s*6} color="red" className="rounded-t-full" width={10} length={radius * 0.85} />
+					<ClockHand deg={h*30} color="#222" className="rounded-t-full" width={20} length={hoursHandLength} />
+					<ClockHand deg={m*6} color="blue" className="rounded-t-full" width={15} length={minutesHandLength} />
+					<ClockHand deg={s*6} color="red" className="rounded-t-full" width={10} length={secondsHandLength} />
 
 					<div className="w-10 h-10 bg-zinc-900 rounded-full shadow -translate-x-1/2 -translate-y-1/2"></div>
 
 					{sixty.map(idx => <Dot key={idx} deg={idx * 6} radius={radius} size={(idx % 5 === 0) ? 16 : 8} />)}
-					{sixty.map(idx => <Dash key={idx} deg={idx * 6} radius={radius * 0.45} />)}
+					{sixty.map(idx => <Dash key={idx} deg={idx * 6} radius={hoursHandLength} />)}
 				</section>
 			</section>
 		</section>
