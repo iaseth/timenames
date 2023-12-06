e3899d7f5393cb9ee7b0a07d4a3eb8e871b88097
Ankur Seth <iankurseth@gmail.com>
2023-12-06 16:09:39 +0530

    Moved dialSize up to Main, can now be passed as a prop to AnalogClock

--- demo/src/App/Main/AnalogClock/AnalogClock.tsx
+++ demo/src/App/Main/AnalogClock/AnalogClock.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ClockHand from "./ClockHand";
 import Dot from "./Dot";
 import Dash from "./Dash";
@@ -6,30 +5,19 @@ import Dash from "./Dash";
 
 
 interface AnalogClockProps {
+	size: number,
 	time: number
 }
 
-export default function AnalogClock ({time}: AnalogClockProps) {
-	const [size, setSize] = React.useState(400);
-	const radius = Math.floor(size / 2);
-
+export default function AnalogClock ({
+	size, time
+}: AnalogClockProps) {
 	const seconds = Math.floor(time / 1000);
 	const s = seconds % 60;
 	const m = seconds / 60;
 	const h = seconds / 3600;
 
-	const updateSize = () => {
-		const wh = window.innerHeight;
-		const ww = window.innerWidth;
-		const min = Math.min(wh, ww);
-		setSize(min);
-	};
-	React.useEffect(() => {
-		updateSize();
-		window.addEventListener('resize', updateSize);
-		return () => window.removeEventListener('resize', updateSize);
-	}, []);
-
+	const radius = Math.floor(size / 2);
 	const style = {
 		height: `${size}px`,
 		width: `${size}px`
@@ -44,14 +32,14 @@ export default function AnalogClock ({time}: AnalogClockProps) {
 		<section className="bg-zinc-100 relative p-4">
 			<section style={style} className="bg-white w-full h-full rounded-full shadow">
 				<section className="absolute top-1/2 left-1/2 w-0 h-0">
+					{sixty.map(idx => <Dot key={idx} deg={idx * 6} radius={radius} size={(idx % 5 === 0) ? 16 : 8} />)}
+					{sixty.map(idx => <Dash key={idx} deg={idx * 6} radius={hoursHandLength} />)}
+
 					<ClockHand deg={h*30} color="#222" className="rounded-t-full" width={20} length={hoursHandLength} />
 					<ClockHand deg={m*6} color="blue" className="rounded-t-full" width={15} length={minutesHandLength} />
 					<ClockHand deg={s*6} color="red" className="rounded-t-full" width={10} length={secondsHandLength} />
 
-					<div className="w-10 h-10 bg-zinc-900 rounded-full shadow -translate-x-1/2 -translate-y-1/2"></div>
-
-					{sixty.map(idx => <Dot key={idx} deg={idx * 6} radius={radius} size={(idx % 5 === 0) ? 16 : 8} />)}
-					{sixty.map(idx => <Dash key={idx} deg={idx * 6} radius={hoursHandLength} />)}
+					<div className="w-10 h-10 bg-zinc-950 rounded-full shadow -translate-x-1/2 -translate-y-1/2"></div>
 				</section>
 			</section>
 		</section>
--- demo/src/App/Main/Main.tsx
+++ demo/src/App/Main/Main.tsx
@@ -11,10 +11,11 @@ interface MainProps {
 }
 
 export default function Main ({}: MainProps) {
+	const [dialSize, setDialSize] = React.useState(320);
 	const [time, setTime] = React.useState(Date.now());
 	const offsetMinutes = new Date().getTimezoneOffset();
 	const offsetMs = offsetMinutes * 60 * 1000;
-	const localTime = time + offsetMs;
+	const localTime = time - offsetMs;
 
 	const updateTime = () => setTime(Date.now());
 	React.useEffect(() => {
@@ -22,11 +23,23 @@ export default function Main ({}: MainProps) {
 		return () => clearInterval(interval);
 	}, []);
 
+	const updateDialSize = () => {
+		const wh = window.innerHeight;
+		const ww = window.innerWidth;
+		const min = Math.min(wh, ww);
+		setDialSize(min);
+	};
+	React.useEffect(() => {
+		updateDialSize();
+		window.addEventListener('resize', updateDialSize);
+		return () => window.removeEventListener('resize', updateDialSize);
+	}, []);
+
 	return (
 		<main className="min-h-screen px-2 py-4 bg-zinc-100 flex">
 			<section className="m-auto text-center rounded shadow overflow-hidden">
 				<Timename time={localTime} />
-				<AnalogClock time={localTime} />
+				<AnalogClock time={localTime} size={dialSize} />
 				<DigitalClock time={localTime} />
 			</section>
 		</main>
